803844034318df6b77b8924f2bb19ddde5fb1fee
arieled91 <arieled91@gmail.com>
2017-08-14 02:10:56 -0300

    fixed war servlet

--- api/src/main/kotlin/ar/edu/usal/smartcity/application/Application.kt
+++ api/src/main/kotlin/ar/edu/usal/smartcity/application/Application.kt
@@ -2,9 +2,16 @@ package ar.edu.usal.smartcity.application
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
 
 @SpringBootApplication
-open class Application {
+open class Application : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(Application::class.java)
+    }
+
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             SpringApplication.run(Application::class.java, *args)
