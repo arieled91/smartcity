aef06586fb5eb5217f1d73fe0e432240824ed4a8
Ariel Cacciamano <acacciamano@tekgenesis.com>
2017-09-06 17:48:24 -0300

    fixed tags

---
+++ api/src/main/kotlin/ar/edu/usal/smartcity/controller/CheckpointController.kt
@@ -0,0 +1,31 @@
+package ar.edu.usal.smartcity.controller
+
+import ar.edu.usal.smartcity.model.city.Checkpoint
+import ar.edu.usal.smartcity.repository.CheckpointRepository
+import ar.edu.usal.smartcity.repository.PlaceRepository
+import ar.edu.usal.smartcity.repository.VehicleRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = "/api")
+class CheckpointController {
+
+    @Autowired lateinit var checkpointRepo: CheckpointRepository
+    @Autowired lateinit var vehicleRepo: VehicleRepository
+    @Autowired lateinit var placeRepo: PlaceRepository
+
+
+    @RequestMapping(value = "/tags", method = arrayOf(RequestMethod.POST))
+    fun saveCheckpoint(@RequestBody request: CheckpointRequest): HttpStatus {
+        val vehicle = vehicleRepo.findByTagId(request.tagCode)
+        val place = placeRepo.findByName(request.placeName)
+        checkpointRepo.save(Checkpoint(vehicle, place))
+        return HttpStatus.OK
+
+    }
+}
--- api/src/main/kotlin/ar/edu/usal/smartcity/controller/RepositoryController.kt
+++ api/src/main/kotlin/ar/edu/usal/smartcity/controller/RepositoryController.kt
@@ -1,11 +1,11 @@
 package ar.edu.usal.smartcity.controller
 
-import ar.edu.usal.smartcity.model.city.Checkpoint
 import ar.edu.usal.smartcity.model.city.TrafficViolation
 import ar.edu.usal.smartcity.model.city.ViolationType
 import ar.edu.usal.smartcity.model.common.Location
 import ar.edu.usal.smartcity.model.common.Resource
-import ar.edu.usal.smartcity.repository.*
+import ar.edu.usal.smartcity.repository.ResourceRepository
+import ar.edu.usal.smartcity.repository.TrafficViolationRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.rest.webmvc.RepositoryRestController
 import org.springframework.http.ResponseEntity
@@ -18,20 +18,8 @@ import java.time.LocalDateTime
 @RepositoryRestController
 class CityController {
 
-    @Autowired lateinit var checkpointRepo: CheckpointRepository
-    @Autowired lateinit var vehicleRepo: VehicleRepository
     @Autowired lateinit var trafficViolRepo: TrafficViolationRepository
     @Autowired lateinit var resourceRepo: ResourceRepository
-    @Autowired lateinit var placeRepo: PlaceRepository
-
-
-    @RequestMapping(value = "/tags", method = arrayOf(RequestMethod.POST))
-    fun saveCheckpoint(@RequestBody request: CheckpointRequest): ResponseEntity<Checkpoint> {
-        val vehicle = vehicleRepo.findByTagId(request.tagCode)
-        val place = placeRepo.findByName(request.placeName)
-        val saved = checkpointRepo.save(Checkpoint(vehicle, place))
-        return ResponseEntity.ok(saved)
-    }
 
     @RequestMapping(value = "/trafficViolations", method = arrayOf(RequestMethod.POST))
     fun saveTrafficViolation(@RequestBody request: TrafficViolationRequest): ResponseEntity<TrafficViolation> {
